09ec60477cd9b4b9a8fafa1f3b440357e50ecce4
E7M2NU3L <aemmanuel685210@gmail.com>
2024-05-16 16:03:31 +0530

    create some White pages

---
+++ app/service/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Service = ({children} : {children : React.ReactNode}) => {
+    return (
+      <div>
+          {children}
+      </div>
+    )
+  }
+
+export default Service
---
+++ app/service/quiz-pro/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuizPro = () => {
+  return (
+    <div>QuizPro</div>
+  )
+}
+
+export default QuizPro
